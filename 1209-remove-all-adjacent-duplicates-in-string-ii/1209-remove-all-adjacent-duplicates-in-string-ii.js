1a138b86d62c310a9735bb1129d65df5b07cbb28
tasangpo <87487268+tasangpo@users.noreply.github.com>
2022-05-02 12:10:23 -0400

    Time: 172 ms (40.46%), Space: 49.3 MB (86.44%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.js
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.js
@@ -3,25 +3,25 @@
  * @param {number} k
  * @return {string}
  */
-var removeDuplicates = function(s, k) {  // "deeedbbcccbdaa" => 'aa'
+var removeDuplicates = function(s, k) {
     const stack = [];
-    for (let char of s) {               // [[d, 1], [e, 1] ]
-        
-        if (!stack.length) {    
+    
+    for (const char of s) {
+        if (stack.length === 0) {
             stack.push([char, 1]);
             continue;
         }
         
-        let [prevChar, prevCount] = stack[stack.length - 1];
-        if (prevChar === char) {
-            const count = prevCount + 1;
-            stack[stack.length - 1][1] = count;
-            if (count === k) stack.pop(); 
+        const [prevChar, prevCount] = stack[stack.length - 1];
+        if (char === prevChar) {
+            stack[stack.length - 1][1] = prevCount + 1;
+            if (stack[stack.length - 1][1] === k) stack.pop();
         } else {
-            stack.push([char, 1])
+            stack.push([char, 1]);
         }
     }
+    
     let res = '';
-    for (const [char, count] of stack) res += char.repeat(count);
+    for (let [char, count] of stack) res += char.repeat(count);
     return res;
 };
