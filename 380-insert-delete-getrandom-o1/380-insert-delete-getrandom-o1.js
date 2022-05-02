231dd7e145f752e5fc59c943e8725f0008e74c62
tasangpo <87487268+tasangpo@users.noreply.github.com>
2022-05-02 15:06:50 -0400

    Time: 952 ms (8.26%), Space: 115.4 MB (19.34%) - LeetHub

---
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.js
@@ -0,0 +1,45 @@
+
+var RandomizedSet = function() {
+  this.set = new Set();  
+};
+
+/** 
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.insert = function(val) {
+    if (this.set.has(val)) return false;
+    this.set.add(val);
+    return true;
+};
+
+/** 
+ * @param {number} val
+ * @return {boolean}
+ */
+RandomizedSet.prototype.remove = function(val) {
+    if (!this.set.has(val)) return false;
+    this.set.delete(val);
+    return true;
+};
+
+/**
+ * @return {number}
+ */
+RandomizedSet.prototype.getRandom = function() {
+    const values = [];
+    for (let ele of this.set) {
+        values.push(ele);
+    }
+    const num = values.length;
+    const randomIndex = Math.floor(Math.random() * num);
+    return values[randomIndex];
+};
+
+/** 
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = new RandomizedSet()
+ * var param_1 = obj.insert(val)
+ * var param_2 = obj.remove(val)
+ * var param_3 = obj.getRandom()
+ */
