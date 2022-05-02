363c719b5374963d82b52196f94c01755009f02c
tasangpo <87487268+tasangpo@users.noreply.github.com>
2022-05-02 13:40:56 -0400

    Time: 1900 ms (24.19%), Space: 112.6 MB (23.38%) - LeetHub

--- 935-knight-dialer/935-knight-dialer.js
+++ 935-knight-dialer/935-knight-dialer.js
@@ -3,42 +3,62 @@
  * @return {number}
  */
 var knightDialer = function(n) {
-    const deltas = [[-2, -1], [-2, 1], [-1, -2], [-1, 2], [1, 2], [1, -2], [2, -1], [2, 1]];
-    let count = 0;
-    const memo = {};
-    const mod = (10**9) + 7;
-    for (let r= 0; r < 4; r++) {
-        for (let c = 0; c < 3; c++) {
-            if (isValid(r, c)) {
-                count += countTotal(n, deltas, r, c, n-1, memo);  
-            }
-        }
-    }
-    
-    return count % mod;
+  // Memo
+  this.memo = new Map();
+  
+  // Modulo
+  this.mod = (10 ** 9) + 7;
+  
+  // All valid knight's directions and valid keypad numeric's coordinates
+  let directions = [[-2, -1], [-2, 1], [-1, 2], [1, 2], [2, 1], [2, -1], [1, -2], [-1, -2]];
+  let numericCoord = [[0,0], [0,1], [0,2], [1,0], [1,1], [1,2], [2,0], [2,1], [2,2], [3, 1]];
+  
+  let count = 0;
+  
+  // Starting coordinate of every valid key in the keypad
+  for (let [r, c] of numericCoord) {
+    count += dp(n, directions, r, c, n - 1);
+  }
+  
+  // Return result with modulo
+  return count % this.mod;
 };
 
-const countTotal = (n, deltas, r, c, remain, memo) => {
-    const pos = r + ',' + c + ',' + remain;
-    
-    if (remain === 0) return 1;
-    
-    if (pos in memo) return memo[pos];
-    
-    let count = 0;
-    const mod = (10**9) + 7;
-    for (let [deltaRow, deltaCol] of deltas) {
-        if (!isValid(r + deltaRow, c + deltaCol)) {
-            continue;
-        }
-        
-        count += countTotal(n, deltas, r + deltaRow, c + deltaCol, remain - 1, memo) % mod;
+var dp = function(n, directions, r, c, remain) {
+  let key = `${r}_${c}_${remain}`;
+  
+  // Base case
+  if (remain === 0) {
+    return 1;
+  }
+  
+  // Return from memo
+  if (this.memo.has(key) === true) {
+    return this.memo.get(key);
+  }
+  
+  let count = 0;
+  
+  // Try to go every valid directions a knight can go
+  for (let [nextR, nextC] of directions) {
+    // If the next direction is invalid, try another direction
+    if (isValid(r + nextR, c + nextC) === false) {
+      continue;
     }
     
-    memo[pos] = count;
-    return memo[pos];
-}
+    // Next direction is valid, result could be huge so we perform modulo
+    count += dp(n, directions, r + nextR, c + nextC, remain - 1) % this.mod;
+  }
+  
+  // Set memo
+  this.memo.set(key, count);
+  
+  return count;
+};
 
-const isValid = (r, c) => {
-    return ((r >= 0 && r < 4 && c >= 0 && c < 3) && !(r === 3 && c === 0) && !(r === 3 && c === 2));
-}
+var isValid = function(r, c) {
+  // Check if coordinate is in keypad coordinate boundary, except the asterisk (3, 0) and hashtag (3, 2)
+  return (r >= 0 && r < 4 && c >= 0 && c < 3) &&
+    !(r === 3 && c === 0) &&
+    !(r === 3 && c === 2);
+};
