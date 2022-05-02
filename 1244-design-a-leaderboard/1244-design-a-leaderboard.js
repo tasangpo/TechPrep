2e986f05235442aaf8947d2b6303474283eb6d6a
tasangpo <87487268+tasangpo@users.noreply.github.com>
2022-05-02 15:59:05 -0400

    Time: 151 ms (28.57%), Space: 46.5 MB (68.25%) - LeetHub

---
+++ 1244-design-a-leaderboard/1244-design-a-leaderboard.js
@@ -0,0 +1,43 @@
+
+var Leaderboard = function() {
+    this.board = {};  // {1: 0, 2: 0, 3:84, 4: 6, 5:89, 6:31, 7:7, 8:1, 9:98, 10:42}
+};
+
+/** 
+ * @param {number} playerId 
+ * @param {number} score
+ * @return {void}
+ */
+Leaderboard.prototype.addScore = function(playerId, score) {
+    if (!(playerId in this.board)) this.board[playerId] = 0;
+    this.board[playerId] += score;
+};
+
+/** 
+ * @param {number} K
+ * @return {number}
+ */
+Leaderboard.prototype.top = function(K) {
+    const score = Object.values(this.board).sort((a, b) => b- a);
+    let total = 0;
+    for (let i = 0; i < K; i++) {
+        total += score[i];
+    }
+    return total;
+};
+
+/** 
+ * @param {number} playerId
+ * @return {void}
+ */
+Leaderboard.prototype.reset = function(playerId) {
+    this.board[playerId] = 0;
+};
+
+/** 
+ * Your Leaderboard object will be instantiated and called as such:
+ * var obj = new Leaderboard()
+ * obj.addScore(playerId,score)
+ * var param_2 = obj.top(K)
+ * obj.reset(playerId)
+ */
