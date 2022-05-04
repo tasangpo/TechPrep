f01a4dac71aafccb4a9597ece9a5b3e2bb0f2e8e
tasangpo <87487268+tasangpo@users.noreply.github.com>
2022-05-04 11:11:10 -0400

    Time: 241 ms (51.58%), Space: 55.2 MB (7.24%) - LeetHub

--- 1656-design-an-ordered-stream/1656-design-an-ordered-stream.js
+++ 1656-design-an-ordered-stream/1656-design-an-ordered-stream.js
@@ -16,7 +16,7 @@ OrderedStream.prototype.insert = function(idKey, value) {
     const output = [];
     while (this.stream[this.current] !== undefined) {
         output.push(this.stream[this.current]);
-        this.current += 1;
+        this.current++;
     }
     return output;
 };
