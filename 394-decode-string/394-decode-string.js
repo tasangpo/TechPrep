e9676804df3b2bf5127fd9473d1c452b591258fb
tasangpo <87487268+tasangpo@users.noreply.github.com>
2022-05-04 11:49:09 -0400

    Time: 104 ms (11.08%), Space: 41.8 MB (84.40%) - LeetHub

--- 394-decode-string/394-decode-string.js
+++ 394-decode-string/394-decode-string.js
@@ -3,30 +3,32 @@
  * @return {string}
  */
 var decodeString = function(s) {
-    const stack = [];           // [3, a, 2, [,  ]
+    const stack = [];               //  ['3']
     const numbers = '0123456789'
     
-    for (const char of s) {   // 3  [   a   2   [   c   ]   ]
-        if (char === ']') {
-            let subString = '';
-            let ele = stack.pop();
-            while (ele !== '[') {
-                subString = ele + subString;
-                ele = stack.pop();
-            }
-            let num = '';
-            let curr = stack.pop();
-            while (numbers.includes(curr)) {
-                num = curr + num;
-                curr = stack.pop();
-            }
-            stack.push(curr);
-            stack.push(subString.repeat(Number(num)))
-                    
-        } else {
+    for (const char of s) {
+        if (char !== ']') {
             stack.push(char);
+            continue;
         }
+        
+        let str = '';               //'a'
+        let ele = stack.pop()
+        while (ele !== '[') {
+            str = ele + str;
+            ele = stack.pop();
+        }
+        
+        let count = '';
+        let num = stack.pop();
+        while (numbers.includes(num)) {
+            count = num + count;
+            num = stack.pop();
+        }
+        stack.push(num);
+        
+        stack.push(str.repeat(count));
     }
-    return stack.join('')
     
+    return stack.join('');
 };
