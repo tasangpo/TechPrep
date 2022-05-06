9fe8562ce3cbe851fb739d9a8168236bd57b62b3
tasangpo <87487268+tasangpo@users.noreply.github.com>
2022-05-06 18:51:40 -0400

    Time: 8986 ms (5.64%), Space: 114.2 MB (22.58%) - LeetHub

--- 935-knight-dialer/935-knight-dialer.js
+++ 935-knight-dialer/935-knight-dialer.js
@@ -3,42 +3,40 @@
  * @return {number}
  */
 var knightDialer = function(n) {
-    const deltas = [[-2, -1], [-2, 1], [-1, -2], [-1, 2], [1, 2], [1, -2], [2, -1], [2, 1]];
+    const mod = 10 ** 9 + 7;
     let count = 0;
-    const memo = {};
-    const mod = (10**9) + 7;
-    for (let r= 0; r < 4; r++) {
+    for (let r = 0; r < 4; r++) {
         for (let c = 0; c < 3; c++) {
-            if (isValid(r, c)) {
-                count += countTotal(n, deltas, r, c, n-1, memo);  
-            }
+            if (validPos(r, c)) count +=  checkMove(n, r, c, n-1)
         }
     }
-    
     return count % mod;
+    
 };
 
-const countTotal = (n, deltas, r, c, remain, memo) => {
-    const pos = r + ',' + c + ',' + remain;
+const validPos = (r, c) => {
+    const rowInbound = 0 <= r && r < 4;
+    const colInbound = 0 <= c && c < 3;
     
-    if (remain === 0) return 1;
-    
-    if (pos in memo) return memo[pos];
-    
-    let count = 0;
-    const mod = (10**9) + 7;
-    for (let [deltaRow, deltaCol] of deltas) {
-        if (!isValid(r + deltaRow, c + deltaCol)) {
-            continue;
-        }
-        
-        count += countTotal(n, deltas, r + deltaRow, c + deltaCol, remain - 1, memo) % mod;
-    }
-    
-    memo[pos] = count;
-    return memo[pos];
+    return (rowInbound && colInbound && !(r === 3 && c == 0) && !(r === 3 && c === 2))
 }
 
-const isValid = (r, c) => {
-    return ((r >= 0 && r < 4 && c >= 0 && c < 3) && !(r === 3 && c === 0) && !(r === 3 && c === 2));
+const checkMove = (n, r, c, remain, memo={}) => {
+    const key = r + ',' + 'c' + remain;
+    if (key in memo) return memo[key];
+    if (remain === 0) return 1;
+    const deltas = [ [1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1] ];
+    const mod = 10 ** 9 + 7;
+
+    let count = 0;
+    for (let [deltaRow, deltaCol] of deltas) {
+        const neighborRow = r + deltaRow;
+        const neighborCol = c + deltaCol;
+        if (!validPos(neighborRow, neighborCol)) continue;
+        
+        count += checkMove(n, neighborRow, neighborCol, remain - 1, memo);
+    }
+    
+    memo[key] = count % mod;
+    return memo[key];
 }
