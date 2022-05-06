5254b2a4396a32dbc90d36f3a07e17d828ddb0c9
tasangpo <87487268+tasangpo@users.noreply.github.com>
2022-05-06 18:55:07 -0400

    Time: 249 ms (66.93%), Space: 50 MB (66.93%) - LeetHub

--- 935-knight-dialer/935-knight-dialer.js
+++ 935-knight-dialer/935-knight-dialer.js
@@ -1,42 +1,49 @@
-/**
- * @param {number} n
- * @return {number}
- */
 var knightDialer = function(n) {
-    const mod = 10 ** 9 + 7;
-    let count = 0;
-    for (let r = 0; r < 4; r++) {
-        for (let c = 0; c < 3; c++) {
-            if (validPos(r, c)) count +=  checkMove(n, r, c, n-1, memo={})
+
+	// if n equals 1, 10 number can be formed, that is with each digit
+    if(n === 1) {
+        return 10;
+    }
+    const mod = 1000000007;
+	
+	// since knight can move in 8 directions, in the dialpad, we know what positions the knight can move when we start from a digit
+	// created the map of those positions that can be moved from key `num`
+    const map = {1: [6,8], 2: [7,9], 3: [4,8], 4: [0,3,9],
+                 5: [], 6: [0,1,7], 7: [2,6], 8: [1,3], 9: [2,4], 0: [4,6]};
+    
+	// create a dp array of size - 10 * (n + 1), initialize with 0
+    const dp = Array(10).fill(null).map(() => Array(n + 1).fill(0));
+	
+	//initialize the dp array for each num and n = 1 and n = 2 
+    let total = 0;
+    for(let num = 0; num <= 9; ++num) {
+        dp[num][1] = 1;
+        dp[num][2] = map[num].length;
+        if(n === 2) {
+            total += dp[num][2];
         }
     }
-    return count % mod;
-    
-};
-
-const validPos = (r, c) => {
-    const rowInbound = 0 <= r && r < 4;
-    const colInbound = 0 <= c && c < 3;
-    
-    return (rowInbound && colInbound && !(r === 3 && c == 0) && !(r === 3 && c === 2))
-}
-
-const checkMove = (n, r, c, remain, memo={}) => {
-    const key = r + ',' + 'c' + remain;
-    if (key in memo) return memo[key];
-    if (remain === 0) return 1;
-    const deltas = [ [1, 2], [1, -2], [-1, 2], [-1, -2], [2, 1], [2, -1], [-2, 1], [-2, -1] ];
-    const mod = 10 ** 9 + 7;
-
-    let count = 0;
-    for (let [deltaRow, deltaCol] of deltas) {
-        const neighborRow = r + deltaRow;
-        const neighborCol = c + deltaCol;
-        if (!validPos(neighborRow, neighborCol)) continue;
-        
-        count += checkMove(n, neighborRow, neighborCol, remain - 1, memo);
+	
+	// if n > 2, we calculate the total numbers that can be formed from each digit when we start from it
+	// `total` will be summation of all the numbers which we will return
+    for(let i = 3; i <= n; ++i) {
+        for(let num = 0; num <= 9; ++num) {
+		
+			/* the number of digits that can be formed starting with `num` equals the sum of the number of digits that can be formed by the digits which are reachable from `num`
+			for instance, for n = 3 and num = 1, `16` and `18` are two 2 digit number. 
+			in order to get the 3 digit numbers starting with `1`, we need to check the digits reachable from 6 and 8
+			for `6` we check all the 2 digit numbers - 3 (0,1,7), and for `8` = 2 (1,3)
+			that is,
+			dp[1][3] = dp[6][2] + dp[8][2]
+			dp[6][2] = 3
+			dp[8][2] = 2
+			=> dp[1][3] = 5 (phone numbers - 160, 161, 167, 181, 183)
+			*/
+            map[num].forEach(d => dp[num][i] = (dp[num][i] + dp[d][i - 1]) % mod);
+            if(i === n) {
+                total = (total + dp[num][i]) % mod;
+            }
+        }    
     }
-    
-    memo[key] = count % mod;
-    return memo[key];
-}
+    return total;
+};
