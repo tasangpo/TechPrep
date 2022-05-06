436a72d165fb9aaa6cb4615d306f884a7b112c4e
tasangpo <87487268+tasangpo@users.noreply.github.com>
2022-05-06 18:16:23 -0400

    Time: 106 ms (14.68%), Space: 43.4 MB (90.79%) - LeetHub

--- 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.js
+++ 430-flatten-a-multilevel-doubly-linked-list/430-flatten-a-multilevel-doubly-linked-list.js
@@ -16,17 +16,19 @@ var flatten = function(head) {
     let curr = head;
     const stack = [];
     while (curr !== null) {
-        if (curr.next === null && stack.length) {
-            curr.next = stack.pop();
-            curr.next.prev = curr;
-        }
-        
+
         if (curr.child) {
             if (curr.next) stack.push(curr.next);
             curr.next = curr.child;
             curr.next.prev = curr;
             curr.child = null;
         }
+        
+                if (curr.next === null && stack.length) {
+            curr.next = stack.pop();
+            curr.next.prev = curr;
+        }
+        
         curr = curr.next;
     }
     
