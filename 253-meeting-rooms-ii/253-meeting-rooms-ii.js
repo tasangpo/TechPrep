973babfed95b28199a40af291712acf4c25ad68f
tasangpo <87487268+tasangpo@users.noreply.github.com>
2022-05-07 17:22:53 -0400

    Time: 83 ms (81.15%), Space: 44.3 MB (79.79%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.js
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.js
@@ -4,20 +4,19 @@
  */
 
     
-var minMeetingRooms = function(intervals) {
-    if(intervals.length<2) return 1;
-    intervals.sort((a,b) => a[0]-b[0]);     //[[0,30],[5,10],[15,20]]
+var minMeetingRooms = function(intervals) {         //  intervals = [[0,30],[5,10],[15,20]]
+    intervals.sort((a,b) => a[0] - b[0])            // sorting by start times
+    const meetingTimes = [intervals[0][1]];         // [ 30 ] there is one meeting going on until 30
     
-    let roomTime = [intervals[0][1]];       // [30]
-    
-    for(let i = 1; i<intervals.length; i++){    
-        let [start,end] = intervals[i];             // [5, 10]
-        let earliest = Math.min(...roomTime);   
-        if(start<earliest)
-            roomTime.push(end);
-        else
-            roomTime[roomTime.indexOf(earliest)] = end
+    for (let i = 1; i < intervals.length; i++) {
+        const [start, end] = intervals[i];
+        const earliest = Math.min(...meetingTimes);   // the earliest meeting that ends is at 30, that means 5 - 10 needs a new room
+        if (start < earliest) {
+            meetingTimes.push(end);     // [30, 10] this means there are two meetings going on that end at 10 and 30;
+        } else {
+            meetingTimes[meetingTimes.indexOf(earliest)] = end;  // this means a meeting started after the earliest one has ended. so you can replace the times
+        }
     }
     
-    return roomTime.length;
+    return meetingTimes.length;
 };
