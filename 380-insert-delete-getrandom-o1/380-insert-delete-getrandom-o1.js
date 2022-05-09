04b1965a2c0c9d1891d42bfe7475e5a3dd4a3f29
tasangpo <87487268+tasangpo@users.noreply.github.com>
2022-05-09 11:12:44 -0400

    Time: 496 ms (60.62%), Space: 107.8 MB (59.67%) - LeetHub

--- 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.js
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.js
@@ -1,45 +1,30 @@
 
-var RandomizedSet = function() {
-  this.set = new Set();  
-};
-
-/** 
- * @param {number} val
- * @return {boolean}
- */
-RandomizedSet.prototype.insert = function(val) {
-    if (this.set.has(val)) return false;
-    this.set.add(val);
-    return true;
-};
-
-/** 
- * @param {number} val
- * @return {boolean}
- */
-RandomizedSet.prototype.remove = function(val) {
-    if (!this.set.has(val)) return false;
-    this.set.delete(val);
-    return true;
-};
-
-/**
- * @return {number}
- */
-RandomizedSet.prototype.getRandom = function() {
-    const values = [];
-    for (let ele of this.set) {
-        values.push(ele);
+class RandomizedSet {
+    constructor() {
+        this.nums = [];
+        this.idxMap = new Map();
     }
-    const num = values.length;
-    const randomIndex = Math.floor(Math.random() * num);
-    return values[randomIndex];
-};
-
-/** 
- * Your RandomizedSet object will be instantiated and called as such:
- * var obj = new RandomizedSet()
- * var param_1 = obj.insert(val)
- * var param_2 = obj.remove(val)
- * var param_3 = obj.getRandom()
- */
+    insert(val) {
+        if(this.idxMap.has(val)) {
+            return false;
+        }
+        this.nums.push(val);
+        this.idxMap.set(val, this.nums.length - 1)
+        return true;
+    }
+    remove(val) {
+        if(!this.idxMap.has(val)) {
+            return false;
+        }
+        const valIdx = this.idxMap.get(val);
+        [this.nums[valIdx], this.nums[this.nums.length - 1]] = [this.nums[this.nums.length - 1], this.nums[valIdx]];
+        this.nums.pop();
+        this.idxMap.set(this.nums[valIdx], valIdx);
+        this.idxMap.delete(val);
+        return true;
+    }
+    getRandom() {
+        const randomIdx = Math.floor(Math.random() * this.nums.length);
+        return this.nums[randomIdx];
+    }
+}
