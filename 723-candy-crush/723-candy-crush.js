74c866b9b02354b0439ffce82d75638c02ef956d
tasangpo <87487268+tasangpo@users.noreply.github.com>
2022-05-09 12:01:27 -0400

    Time: 132 ms (36.45%), Space: 45 MB (56.07%) - LeetHub

--- 723-candy-crush/723-candy-crush.js
+++ 723-candy-crush/723-candy-crush.js
@@ -5,65 +5,62 @@
 var candyCrush = function(board) {
     let needCrush = false;
     
-    //check and tag row;
     for (let r = 0; r < board.length; r++) {
         let start = 0;
-        for (let c = 0; c <board[0].length; c++) {
-            if (c - start >= 2) {                           // making sure im at the 3rd col to check the row up to 3 spaces back
-                const num3 = Math.abs(board[r][c]);
+        for (let c = 0; c < board[0].length; c++) {
+            if (c - start >= 2) {
+                const num1 = Math.abs(board[r][c]);
                 const num2 = Math.abs(board[r][c - 1]);
-                const num1 = Math.abs(board[r][c - 2]);
-                
-                if (num3 === num2 && num2 === num1 && num1 !== 0) {
-                    board[r][c] = -num3;
+                const num3 = Math.abs(board[r][c - 2]);
+                if (num1 === num2 && num2 === num3 && num3 !== 0) {
+                    board[r][c] = -num1;
                     board[r][c - 1] = -num2;
                     board[r][c - 2] = -num3;
                     needCrush = true;
                 }
                 start++;
+
             }
         }
     }
     
-    // check and tag column;
     for (let c = 0; c < board[0].length; c++) {
         let start = 0;
         for (let r = 0; r < board.length; r++) {
             if (r - start >= 2) {
-                const num3 = Math.abs(board[r][c]);
+                const num1 = Math.abs(board[r][c]);
                 const num2 = Math.abs(board[r - 1][c]);
-                const num1 = Math.abs(board[r - 2][c]);
-            
-            
-                if (num3 === num2 && num2 === num1 && num1 !== 0) {
-                    board[r][c] = -num3;
+                const num3 = Math.abs(board[r - 2][c]);
+                if (num1 === num2 && num2 === num3 && num3 !== 0) {
+                    board[r][c] = -num1;
                     board[r - 1][c] = -num2;
-                    board[r - 2][c] = -num1;
+                    board[r - 2][c] = -num3;
                     needCrush = true;
                 }
                 start++;
+
             }
         }
     }
     
-    
-    // apply gravity;
-    for (let c = 0; c <board[0].length; c++) {
-        let index = board.length - 1;
-        
+   
+    for (let c = 0; c < board[0].length; c++) {
+        let idx = board.length - 1;
         for (let r = board.length - 1; r >= 0; r--) {
-            // this will shift everything down
             if (board[r][c] > 0) {
-                board[index][c] = board[r][c];
-             index -= 1;
+                [board[idx][c], board[r][c]] = [board[r][c], board[idx][c]];
+                idx--;
             }
         }
         
-        // fill the rest of cols with 0 after the index
-        for (let r = index; r >= 0; r--) {
-            board[r][c] = 0;
+        for (let r = idx; r >= 0; r--) {
+            board[r][c] = 0
         }
     }
     
-    return !needCrush ? board : candyCrush(board);
+    
+    
+    
+    
+    return needCrush ? candyCrush(board) : board;
 };
