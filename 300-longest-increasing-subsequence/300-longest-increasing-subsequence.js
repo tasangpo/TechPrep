191d632c7bc370b412d5b015f7e244a3700043ca
tasangpo <87487268+tasangpo@users.noreply.github.com>
2022-05-09 18:57:06 -0400

    Time: 238 ms (33.18%), Space: 43.9 MB (61.48%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function (nums) {
+    const LIS = new Array(nums.length).fill(1);
+    for (let i = nums.length - 1; i > -1; i--) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] < nums[j]) {
+                LIS[i] = Math.max(LIS[i], 1 + LIS[j]);
+            }
+        }
+    }
+    return Math.max(...LIS);
+};
