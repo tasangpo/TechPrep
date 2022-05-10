69d55e3fdfad34db0d79a089caa82458ecbbc233
tasangpo <87487268+tasangpo@users.noreply.github.com>
2022-05-09 20:41:08 -0400

    Time: 188 ms (16.98%), Space: 52.1 MB (33.12%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.js
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.js
@@ -2,20 +2,22 @@
  * @param {number[][]} graph
  * @return {number[][]}
  */
-var allPathsSourceTarget = function(graph) {
+var allPathsSourceTarget = function(graph, i = 0) {
     const paths = [];
-    traverse(0, graph, paths, [] )
+    traverse(i, graph, paths, []);
     return paths;
 };
 
 const traverse = (i, graph, paths, path) => {
     path.push(i);
-    
     if (i === graph.length - 1) {
-        paths.push(path)
-        return
+        paths.push(path);
+        return;
     }
+    
     for (let neighbor of graph[i]) {
         traverse(neighbor, graph, paths, [...path])
     }
+    
+    return paths;
 }
