357150762fef4f590d85a9afb84e1e79abd1bfe4
tasangpo <87487268+tasangpo@users.noreply.github.com>
2022-05-10 12:42:38 -0400

    Time: 520 ms (53.77%), Space: 108.5 MB (55.11%) - LeetHub

--- 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.js
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.js
@@ -1,32 +1,49 @@
 
 var RandomizedSet = function() {
     this.map = new Map();
-    this.array = [];
+    this.nums = [];
 };
 
+/** 
+ * @param {number} val
+ * @return {boolean}
+ */
 RandomizedSet.prototype.insert = function(val) {
     if (this.map.has(val)) return false;
-    this.array.push(val);
-    this.map.set(val, this.array.length - 1);
+    this.nums.push(val);
+    this.map.set(val, this.nums.length - 1);
     return true;
 };
 
-
+/** 
+ * @param {number} val
+ * @return {boolean}
+ */
 RandomizedSet.prototype.remove = function(val) {
     if (!this.map.has(val)) return false;
-    
     const idx = this.map.get(val);
-    
-    [this.array[idx], this.array[this.array.length - 1]] = [this.array[this.array.length - 1], this.array[idx]];
-    this.array.pop();
-    this.map.set(this.array[idx], idx)
+    [this.nums[idx], this.nums[this.nums.length - 1]] = [this.nums[this.nums.length - 1], this.nums[idx]];
+    this.nums.pop();
+    this.map.set(this.nums[idx], idx);
     this.map.delete(val);
     return true;
+    
+
 };
 
-
+/**
+ * @return {number}
+ */
 RandomizedSet.prototype.getRandom = function() {
-    const randomIdx = Math.floor(Math.random() * this.array.length);
-    return this.array[randomIdx]
+    const random = Math.floor(Math.random() * this.nums.length);
+    return this.nums[random];
+    
 };
 
+/** 
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = new RandomizedSet()
+ * var param_1 = obj.insert(val)
+ * var param_2 = obj.remove(val)
+ * var param_3 = obj.getRandom()
+ */
