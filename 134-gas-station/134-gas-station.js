fef4aa5a9538dfe666753263387cd56af8b324b6
tasangpo <87487268+tasangpo@users.noreply.github.com>
2022-05-10 11:17:37 -0400

    Time: 120 ms (37.89%), Space: 50.4 MB (86.33%) - LeetHub

--- 134-gas-station/134-gas-station.js
+++ 134-gas-station/134-gas-station.js
@@ -3,37 +3,37 @@
  * @param {number[]} cost
  * @return {number}
  */
-// var canCompleteCircuit = function(gas, cost) {
-//     let totalTank = 0;
-//     let currentTank = 0;
-//     let startStation = 0;
-    
-//     for (let i = 0; i < gas.length; i++) {
-//         const net = gas[i] - cost[i];
-//         currentTank += net;
-//         totalTank += net;
-//         if (currentTank < 0) {
-//             startingStation = i + 1;
-//             currentTank = 0;
-//         }
-//     }
-    
-//     return totalTank < 0 ? -1 : startingStation;
-// };
-
-
 var canCompleteCircuit = function(gas, cost) {
     let totalTank = 0;
     let currentTank = 0;
-    let startingStation = 0;
+    let startStation = 0;
+    
     for (let i = 0; i < gas.length; i++) {
-        const netCost = gas[i] - cost[i];
-        totalTank += netCost;
-        currentTank += netCost;
+        const net = gas[i] - cost[i];
+        currentTank += net;
+        totalTank += net;
         if (currentTank < 0) {
-            startingStation = i+1;
+            startStation = i + 1;
             currentTank = 0;
         }
     }
-    return totalTank < 0 ? -1 : startingStation;
+    
+    return totalTank < 0 ? -1 : startStation;
 };
+
+
+// var canCompleteCircuit = function(gas, cost) {
+//     let totalTank = 0;
+//     let currentTank = 0;
+//     let startingStation = 0;
+//     for (let i = 0; i < gas.length; i++) {
+//         const netCost = gas[i] - cost[i];
+//         totalTank += netCost;
+//         currentTank += netCost;
+//         if (currentTank < 0) {
+//             startingStation = i+1;
+//             currentTank = 0;
+//         }
+//     }
+//     return totalTank < 0 ? -1 : startingStation;
+// };
