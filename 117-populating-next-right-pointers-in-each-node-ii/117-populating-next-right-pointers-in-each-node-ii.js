8858e6e0482674c1e472243b3f51f8980aa851b9
tasangpo <87487268+tasangpo@users.noreply.github.com>
2022-05-12 11:02:30 -0400

    Time: 89 ms (67.01%), Space: 46.8 MB (27.30%) - LeetHub

---
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.js
@@ -0,0 +1,34 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var connect = function(root) {
+    if (root === null) return null;
+    
+    const queue = [ [root] ];
+    while (queue.length) {
+        const nodes = queue.pop();
+        const newNodes = [];
+        
+        for (let i = 0; i < nodes.length; i++) {
+            nodes[i].next = nodes[i + 1] || null;
+            if (nodes[i].left) newNodes.push(nodes[i].left);
+            if (nodes[i].right) newNodes.push(nodes[i].right);
+        };
+        
+        if (newNodes.length) queue.push(newNodes);
+        
+    };
+    
+    return root;
+};
