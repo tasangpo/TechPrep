3d3c04e8be2622f4fa48fea7b8bac9ce8b913414
tasangpo <87487268+tasangpo@users.noreply.github.com>
2022-05-13 09:43:26 -0400

    Time: 96 ms (60.44%), Space: 44.5 MB (72.85%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.js
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.js
@@ -2,21 +2,18 @@
  * @param {number[][]} intervals
  * @return {number}
  */
-
+var minMeetingRooms = function(intervals) {
+    intervals.sort((a,b) => a[0] - b[0]);
     
-var minMeetingRooms = function(intervals) {         //  intervals = [[0,30],[5,10],[15,20]]
-    intervals.sort((a,b) => a[0] - b[0])            // sorting by start times
-    const meetingTimes = [intervals[0][1]];         // [ 30 ] there is one meeting going on until 30
-    
-    for (let i = 1; i < intervals.length; i++) {
+    const times = [intervals[0][1]];
+    for (let i = 1; i < intervals.length; i++)  {
         const [start, end] = intervals[i];
-        const earliest = Math.min(...meetingTimes);   // the earliest meeting that ends is at 30, that means 5 - 10 needs a new room
-        if (start < earliest) {
-            meetingTimes.push(end);     // [30, 10] this means there are two meetings going on that end at 10 and 30;
+        const earliestEnd = Math.min(...times);
+        if (start < earliestEnd) {
+            times.push(end);
         } else {
-            meetingTimes[meetingTimes.indexOf(earliest)] = end;  // this means a meeting started after the earliest one has ended. so you can replace the times
+            times[times.indexOf(earliestEnd)] = end;
         }
     }
-    
-    return meetingTimes.length;
+    return times.length;
 };
