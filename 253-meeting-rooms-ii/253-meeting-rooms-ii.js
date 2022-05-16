41099b901f3bf8397847286e5d52975ba0deab5b
tasangpo <87487268+tasangpo@users.noreply.github.com>
2022-05-16 00:30:22 -0400

    Time: 72 ms (95.60%), Space: 44.9 MB (42.07%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.js
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.js
@@ -3,21 +3,22 @@
  * @return {number}
  */
 var minMeetingRooms = function(intervals) {
-    const startTimes = intervals.map(interval => interval[0]).sort((a,b) => a - b); // [1,13]
-    const endTimes = intervals.map(interval => interval[1]).sort((a,b) => a - b);   // [13,15]
+    const startTimes = intervals.map(int => int[0]).sort((a,b)=> a - b);        // [0, 5, 15]
+    const endTimes = intervals.map(int => int[1]).sort((a, b) => a - b);        // [10, 20, 30]
     
     let startIdx = 0;
     let endIdx = 0;
     let count = 0;
     
-    
     while (startIdx < intervals.length) {
         if (startTimes[startIdx] < endTimes[endIdx]) {
             count++;
-        } else if (startTimes[startIdx] >= endTimes[endIdx]) {
+        } else {
             endIdx++;
-        } 
+        }
+        
         startIdx++;
     }
+    
     return count;
 };
