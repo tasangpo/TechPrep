3db0e41651f89fec52edfb30c6af2104f31d679d
tasangpo <87487268+tasangpo@users.noreply.github.com>
2022-05-16 15:31:05 -0400

    Time: 89 ms (55.17%), Space: 44.2 MB (100.00%) - LeetHub

--- 1274-number-of-ships-in-a-rectangle/1274-number-of-ships-in-a-rectangle.js
+++ 1274-number-of-ships-in-a-rectangle/1274-number-of-ships-in-a-rectangle.js
@@ -24,15 +24,10 @@ var countShips = function(sea, topRight, bottomLeft) {
     const midX = Math.floor((topRight[0] + bottomLeft[0]) / 2);
     const midY = Math.floor((topRight[1] + bottomLeft[1]) / 2);
     
-    
     const bottomLeftQuad = countShips(sea, [midX, midY], bottomLeft);
-    const bottomRightQuad = countShips(sea, [topRight[0], midY], [midX + 1, bottomLeft[1]] );
+    const bottomRightQuad = countShips(sea, [topRight[0], midY], [midX + 1, bottomLeft[1]]);
     const topLeftQuad = countShips(sea, [midX, topRight[1]], [bottomLeft[0], midY + 1]);
     const topRightQuad = countShips(sea, topRight, [midX + 1, midY + 1]);
     
     return bottomLeftQuad + bottomRightQuad + topLeftQuad + topRightQuad;
 };
-
-
-    
-    // return  countShips(sea, [topRight[0], midY], [midX + 1, bottomLeft[1]]) + countShips(sea, [midX, topRight[1]], [bottomLeft[0], midY + 1]) + countShips(sea, topRight, [midX + 1, midY + 1]);
