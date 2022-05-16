ea4caa3fc9e98fafb91de503b0c8a1756009c340
tasangpo <87487268+tasangpo@users.noreply.github.com>
2022-05-16 15:41:45 -0400

    Time: 162 ms (33.54%), Space: 51.7 MB (32.57%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.js
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.js
@@ -3,8 +3,9 @@
  * @param {number} k
  * @return {string}
  */
-var removeDuplicates = function(s, k) {  // "deeedbbcccbdaa", k = 3
-    const stack = [];                   //  [ ['a',2] ]
+var removeDuplicates = function(s, k) {
+    const stack = [];       //  [[a,1], [b, 1], [c, 1], [d, 1]]
+    
     for (const char of s) {
         if (!stack.length) {
             stack.push([char, 1]);
@@ -20,16 +21,10 @@ var removeDuplicates = function(s, k) {  // "deeedbbcccbdaa", k = 3
                 stack[stack.length - 1][1] = newCount;
             }
         } else {
-            stack.push([char, 1]);
+            stack.push([char, 1])
         }
         
-    }
+    };
     
-    let res = '';
-    
-    for (const [char, count] of stack) {
-        res += char.repeat(count);
-    }
-    
-    return res;
+    return stack.map(arr => arr[0].repeat(arr[1])).join('')
 };
