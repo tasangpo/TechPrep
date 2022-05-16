07e96e9769d6b7d310420dad4ae20109e8ee397e
tasangpo <87487268+tasangpo@users.noreply.github.com>
2022-05-15 22:44:51 -0400

    Time: 69 ms (76.79%), Space: 42.2 MB (65.53%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.js
+++ 1029-two-city-scheduling/1029-two-city-scheduling.js
@@ -3,9 +3,12 @@
  * @return {number}
  */
 var twoCitySchedCost = function(costs) {
-    costs.sort((a,b) => (a[0]-a[1]) - (b[0]-b[1]));
+    costs.sort((a,b) => (a[0] - a[1]) - (b[0] - b[1]));
     const mid = Math.floor(costs.length / 2);
+    
     let total = 0;
-    for (let i = 0; i < mid; i++) total += costs[i][0] + costs[mid+i][1];
+    for (let i = 0; i < mid; i++) {
+        total += costs[i][0] + costs[mid + i][1]; 
+    }
     return total;
 };
