3b83685538447523d86f9d272ab8e4cb4ecc64b7
tasangpo <87487268+tasangpo@users.noreply.github.com>
2022-05-16 22:14:28 -0400

    Time: 51 ms (99.22%), Space: 42.3 MB (23.65%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.js
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.js
@@ -2,34 +2,37 @@
  * @param {string} digits
  * @return {string[]}
  */
-
 const phone = {
     2: 'abc',
     3: 'def',
     4: 'ghi',
-    5: 'jkl',
+    5: 'jkl', 
     6: 'mno',
     7: 'pqrs',
     8: 'tuv',
     9: 'wxyz'
-}
-
-var letterCombinations = function(digits) {
-    if (!digits) return [];
-    
-    const combos = [];
-    generateCombos(0, digits, combos, curr=[]);
-    return combos;
 };
 
-const generateCombos = (i, digits, combos, curr) => {
+
+var letterCombinations = function(digits) {
+    if (!digits.length) return [];
+    const all = [];
+    
+    generateComb(0, digits, all, []);
+    
+    return all;
+};
+
+
+const generateComb = (i, digits, all, curr) => {
     if (i === digits.length) {
-        combos.push(curr.join(''));
+        all.push(curr.join(''));
         return;
     }
     
     for (const char of phone[digits[i]]) {
-        generateCombos(i + 1, digits, combos, [...curr, char])
+        generateComb(i + 1, digits, all, [...curr, char]);
     }
+    
+    
 }
-
